25189c09451c56c4b4ff05f96be97f0fe3b52cf5
James Mc Manus <s00178697@mmoffattws.campus.itsligo.ie>
2018-03-15 11:16:33 +0000

    first commit

---
+++ hygrometer/hygrometer.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+
+overview = DHT.read11(11);
+temp = DHT.temperature,1;
+hum = DHT.humidity,1;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
